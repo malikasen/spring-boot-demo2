d420d815f5b9caa06f75f7d420a94e1b04282e4c
Malika <malikasenova@gmail.com>
2021-12-07 22:33:41 -0800

    Added data to database

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,13 +1,8 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -18,10 +13,5 @@ public class StudentController {
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
-    }
-
-    @GetMapping
-    public List<Student> hello() {
-        return studentService.getStudents();
     }
 }
